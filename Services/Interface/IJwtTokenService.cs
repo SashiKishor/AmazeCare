b2a865fc7f5121b96337360931981471a77131d4
SashiKishor <120318552+SashiKishor@users.noreply.github.com>
2026-07-08 16:27:33 +0530

    Delete Services directory

--- Services/Interface/IJwtTokenService.cs
+++
@@ -1,9 +0,0 @@
-﻿using AmazeCareWebApi.Models;
-
-namespace AmazeCareWebApi.Services.Interface
-{
-    public interface IJwtTokenService
-    {
-        string GenerateToken(User user, out DateTime expiresAt);
-    }
-}
